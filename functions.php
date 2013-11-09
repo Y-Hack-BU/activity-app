cc0aacc376420e23f3d529f9b0cabd7e5231c163
John Moore <john@programiscellaneous.com>
2013-11-09 05:33:14 -0500

    function prototypes

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?PHP
 require_once("config.php");
-function UpdateFriendsList($facebook) {
+function UpdateFriendsLists($facebook) {
 	$user_id = $facebook->getUser();
 	try {
 		$params = array(
@@ -14,12 +14,12 @@ function UpdateFriendsList($facebook) {
 			$flid = $r['flid'];
 			$query = mysql_query("SELECT id FROM groups WHERE flid = $flid");
 			if (!$query) {
-				die("Query error 1");
+				die("Query error");
 			}
 			if (mysql_num_rows($query) < 1) {
 				$query = mysql_query("INSERT INTO groups (fb_uid, flid, name) VALUES ($user_id, $flid, '".$name."');");
 				if (!$query) {
-					die("Query error 2");
+					die("Query error");
 				}
 
 			}
@@ -32,7 +32,7 @@ function UpdateFriendsList($facebook) {
 				$uid = $uid['uid'];
 				$query = mysql_query("INSERT INTO `group_mems` (flid,fid) SELECT $flid, $uid FROM DUAL WHERE NOT EXISTS (SELECT 1 FROM `group_mems` WHERE flid=$flid AND fid=$uid) LIMIT 1");
 				if (!$query) {
-					die("Query error 3".mysql_error());
+					die("Query error");
 				}
 			}
 		}
@@ -46,9 +46,9 @@ function RegisterIfNotExists($facebook) {
 	$query = mysql_query("SELECT id FROM users WHERE fb_uid = ".$user_id);
 	if (mysql_num_rows($query) < 1) {
       //Need to register user
-		$query = mysql_query("INSERT INTO users (fb_uid, fname, lname, txt, cellnum) VALUES (".$user_id.", '".$user_profile['first_name']."', '".$user_profile['last_name']."', 0, 0);");
+		$query = mysql_query("INSERT INTO users (fb_uid, fname, lname, txten, cellnum) VALUES (".$user_id.", '".$user_profile['first_name']."', '".$user_profile['last_name']."', 0, 0);");
 		if (!$query) {
-			die("Error with query");
+			die("Query error");
 		} else {
 			print "User registered!!";
 		}
@@ -62,7 +62,46 @@ function SetUpSQL() {
 	}
 	$query = mysql_query("USE ".SQL_DB.";");
 	if (!$query) {
-		die("Error selecting database");
+		die("Query error");
 	}
+}
+
+function DeletePing($user_id, $pingid) {
+
+}
+
+function GetMyPendingPings($user_id) {
+	//GetUnexpiredPings($user_id)
+}
+
+function InsertPing($user_id, $type, $detail, $start, $duration) {
+
+}
+
+function GetUnexpiredPings($user_id) {
+	
+	//SELECT * FROM pings WHERE start+duration > currenttime AND owner_uid = $result
+}
+
+function GetAllFriendlyPings($user_id) {
+	//SELECT flid FROM group_mems WHERE fid = $user_id
+	//foreach flid as $result:
+		//SELECT fb_uid FROM groups WHERE flid = $result
+		//GetUnexpiredPings(fb_uid)
+		//append $results as potential matching pings
+	//return results
+}
+
+function GetMatchingPings($user_id) {
+	//GetAllFriendlyPings as potential pings
+	//SELECT * from pings WHERE owner_uid = $user_id
+	//foreach result:
+		//foreach potential ping:
+			//if types are equal and times overlap, add match to database and add to return values
+	//return return values
+}
+
+function InsertPingMatch($pid1, $pid2, $uid1, $uid2) {
+
 }
 ?>
---
+++ time_entry.html
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)" 
+		href="styles/mobile.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<!-- Header -->
+		<div class = "header">
+			<div class = "header_logo">
+				<img src = "../img/pinguin_logo.png" />
+				<span>Pinguin</span>
+			</div>
+		</div>
+		
+		<div class = "add_details">
+			<span>Do you want to add any details?</span>
+			<input type = "text" class = "details_question_input" line = "2"/>
+		</div>
+		
+		<div class = "when">
+			
+			<span>When do you want to do this?</span>
+			
+			<div class = "now_or_later">
+				<input type="radio" name="group1" value="Now"> Now
+				<input type="radio" name="group1" value="Later" checked> Later<br>
+			</div>
+			
+			<div class = "when_start_time">
+				<input type="date" class = "start_date_entry"/>
+				<input type="time" class = "start_time_entry"/>
+			</div>
+			
+			<div class = "when_end_time">
+				<input type="date" class = "end_date_entry"/>
+				<input type="time" class = "end_time_entry"/>
+			</div>
+			
+		</div>
+		
+		<div class = "when_next_button">
+				<span>Next</span>
+		</div>
+		
+	</body>
+</html>
